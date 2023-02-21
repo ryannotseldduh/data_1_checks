9621eb49024c0cc67db02010163ae9bbda5d2882
Ryan Huddleston <rhuddlest@gmail.com>
2023-02-21 16:12:01 -0500

    greeting.py

---
+++ greeting.py
@@ -0,0 +1,19 @@
+# This is my salutation 
+
+def greeting():
+    print('hello world')
+    
+greeting()
+ 
+ #This is a list of languages 
+
+languages = ["German", "Quechua", "Awabakal", "Chinese", "Urdu", "HTML"]
+print(languages[5])
+
+#dictionary for languages
+dict = {"European": "German", "American Indigenous": "Quechua", "Aboriginal Australian": "Awabakal", "East Asian": "Chinese", "South Asian": "Urdu", "Coding Language": "HTML"}
+print(dict ["East Asian"])
+
+#tuple with values 
+alphabet_tuple = ("Latin", "Greek", "Cyrillic", "Kanji")
+print(alphabet_tuple[0])
